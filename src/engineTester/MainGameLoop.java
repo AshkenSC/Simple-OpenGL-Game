c036e2b7e1aaddaf29008a2fb0233a4af42884e6
Ashken Aye <393940378@qq.com>
2018-02-18 23:54:23 +0800

    Added specular lighting
    Added specular lighting, but the model still cannot be displayed correctly. The problem is yet to be resloved.

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -31,6 +31,9 @@ public class MainGameLoop {
 		//TexturedModel texturedModel = new TexturedModel(model, texture);
 		//strangely different from previous code...
 		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("yellow")));
+		ModelTexture texture = staticModel.getTexture();
+		texture.setShineDamper(10);
+		texture.setReflectivity(1);
 		
 		Entity entity = new Entity(staticModel, new Vector3f(0, -3, -25), 0, 0, 0, 1);
 		Light light = new Light(new Vector3f(0, 0, -20), new Vector3f(1, 1, 1));
