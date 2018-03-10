0b0d0257062554a16f6cb8febe2eeb093eae2808
Ashken Aye <393940378@qq.com>
2018-03-10 23:20:55 +0800

    Terrain lighting effect update
    Updated normals of terrain to better reflect light.
    Tweaked sky colour.

--- src/renderEngine/MasterRenderer.java
+++ src/renderEngine/MasterRenderer.java
@@ -23,9 +23,9 @@ public class MasterRenderer {
 	private static final float NEAR_PLANE = 0.1f;
 	private static final float FAR_PLANE = 1000.0f;
 
-	private static final float RED = 0.5f;
-	private static final float GREEN = 0.8f;
-	private static final float BLUE = 0.9f;
+	private static final float RED = 0.9f;
+	private static final float GREEN = 1.0f;
+	private static final float BLUE = 1.0f;
 	
 	private Matrix4f projectionMatrix;
 	
--- src/terrains/Terrain.java
+++ src/terrains/Terrain.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import org.lwjgl.util.vector.Vector3f;
+
 import models.RawModel;
 import renderEngine.Loader;
 import textures.ModelTexture;
@@ -79,9 +81,10 @@ public class Terrain {
 				vertices[vertexPointer*3] = (float)j/((float)VERTEX_COUNT - 1) * SIZE;
 				vertices[vertexPointer*3+1] = getHeight(j, i, image);
 				vertices[vertexPointer*3+2] = (float)i/((float)VERTEX_COUNT - 1) * SIZE;
-				normals[vertexPointer*3] = 0;
-				normals[vertexPointer*3+1] = 1;
-				normals[vertexPointer*3+2] = 0;
+				Vector3f normal = calculateNormal(j, i, image);
+				normals[vertexPointer*3] = normal.x;
+				normals[vertexPointer*3+1] = normal.y;
+				normals[vertexPointer*3+2] = normal.z;
 				textureCoords[vertexPointer*2] = (float)j/((float)VERTEX_COUNT - 1);
 				textureCoords[vertexPointer*2+1] = (float)i/((float)VERTEX_COUNT - 1);
 				vertexPointer++;
@@ -103,6 +106,17 @@ public class Terrain {
 			}
 		}
 		return loader.loadToVAO(vertices, textureCoords, normals, indices);
+	}
+	
+	private Vector3f calculateNormal(int x, int z, BufferedImage image)
+	{
+		float heightL = getHeight(x-1, z, image);
+		float heightR = getHeight(x+1, z, image);
+		float heightD = getHeight(x, z-1, image);
+		float heightU = getHeight(x, z+1, image);
+		Vector3f normal = new Vector3f(heightL-heightR, 2f, heightD - heightU);
+		normal.normalise();
+		return normal;
 	}
 	
 	private float getHeight(int x, int z, BufferedImage image)
