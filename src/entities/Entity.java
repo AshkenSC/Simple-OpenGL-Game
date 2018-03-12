4d9a69b9e693acaba7230bd52ce2e36c7714c37c
Ashken Aye <393940378@qq.com>
2018-03-12 23:46:59 +0800

    Update plant texture diversity (undone)
    1) Using texture atlas to improve texture diversity, but the code is not finished.
    2) Collision detection of the other half of terrain is still yet to be finished.

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -11,14 +11,36 @@ public class Entity {
 	private float rotX, rotY, rotZ;
 	private float scale;
 	
+	private int textureIndex = 0;
+	
 	public Entity(TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ, float scale) {
-		//super();
+		
 		this.model = model;
 		this.position = position;
 		this.rotX = rotX;
 		this.rotY = rotY;
 		this.rotZ = rotZ;
 		this.scale = scale;
+	}
+	
+	public Entity(TexturedModel model, int index, Vector3f position, float rotX, float rotY, float rotZ, float scale) {
+		this.textureIndex = index;
+		this.model = model;
+		this.position = position;
+		this.rotX = rotX;
+		this.rotY = rotY;
+		this.rotZ = rotZ;
+		this.scale = scale;
+	}
+	
+	public float getTextureXOffset() {
+		int column = textureIndex % model.getTexture().getNumberOfRows();
+		return (float)column / (float)model.getTexture().getNumberOfRows();
+	}
+	
+	public float getTextureYOffset() {
+		int row = textureIndex / model.getTexture().getNumberOfRows();
+		return (float)row / (float)model.getTexture().getNumberOfRows();
 	}
 	
 	public void increaseRotation(float dx, float dy, float dz)
--- src/renderEngine/EntityRenderer.java
+++ src/renderEngine/EntityRenderer.java
@@ -51,6 +51,7 @@ public class EntityRenderer {
 		GL20.glEnableVertexAttribArray(1);
 		GL20.glEnableVertexAttribArray(2);
 		ModelTexture texture = model.getTexture();
+		shader.loadNumberOfRows(texture.getNumberOfRows());
 		if(texture.isHasTransparency())
 		{
 			MasterRenderer.disableCulling();
@@ -75,6 +76,7 @@ public class EntityRenderer {
 		Matrix4f transformationMatrix = Maths.createTransformationMatrix(entity.getPosition(),
 				entity.getRotX(), entity.getRotY(), entity.getRotZ(), entity.getScale());
 		shader.loadTransformationMatrix(transformationMatrix);
+		shader.loadOffset(entity.getTextureXOffset(), entity.getTextureYOffset());
 	}
 		
 }
