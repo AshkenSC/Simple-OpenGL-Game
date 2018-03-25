6ee397927081a3884b16e6b3f5337357d9f935e4
Ashken Aye <393940378@qq.com>
2018-03-25 23:40:04 +0800

    Adding GUI to the game(in progress)
    After some bug fixes, GUI textures can now be loaded. But some warning messages still exist.

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -109,7 +109,7 @@ public class MainGameLoop {
 		
 		// load GUI
 		List<GuiTexture> guis = new ArrayList<GuiTexture>();
-		GuiTexture gui = new GuiTexture(loader.loadTexture("image"), new Vector2f(0.5f, 0.5f), new Vector2f(0.25f, 0.25f));
+		GuiTexture gui = new GuiTexture(loader.loadTexture("hots"), new Vector2f(0.5f, 0.5f), new Vector2f(0.25f, 0.25f));
 		guis.add(gui);
 		
 		GuiRenderer guiRenderer = new GuiRenderer(loader);
@@ -143,6 +143,7 @@ public class MainGameLoop {
 			DisplayManager.updateDisplay();
 		}
 		
+		guiRenderer.cleanUp();
 		renderer.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
