125369e5a73dbafd86abbc0dd7af94791cc769e0
AshkenSC <393940378@qq.com>
2018-05-06 23:50:44 +0800

    Added simple cloud moving effect
    The skybox is now rotating to simulate that the clouds are moving across
    the sky.

--- src/skybox/SkyboxShader.java
+++ src/skybox/SkyboxShader.java
@@ -4,7 +4,7 @@ import org.lwjgl.util.vector.Matrix4f;
 import org.lwjgl.util.vector.Vector3f;
 
 import entities.Camera;
- 
+import renderEngine.DisplayManager;
 import shaders.ShaderProgram;
 import toolbox.Maths;
  
@@ -12,10 +12,14 @@ public class SkyboxShader extends ShaderProgram{
  
     private static final String VERTEX_FILE = "src/skybox/skyboxVertexShader.txt";
     private static final String FRAGMENT_FILE = "src/skybox/skyboxFragmentShader.txt";
-     
+    
+    private static final float ROTATE_SPEED = 1f;
+    
     private int location_projectionMatrix;
     private int location_viewMatrix;
     private int location_fogColour;
+    
+    private float rotation = 0;
      
     public SkyboxShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
@@ -30,6 +34,8 @@ public class SkyboxShader extends ShaderProgram{
         matrix.m30 = 0;
         matrix.m31 = 0;
         matrix.m32 = 0;
+        rotation += ROTATE_SPEED * DisplayManager.getFrameTimeSeconds();
+        Matrix4f.rotate((float) Math.toRadians(rotation), new Vector3f(0, 1, 0), matrix, matrix);
         super.loadMatrix(location_viewMatrix, matrix);
     }
     
