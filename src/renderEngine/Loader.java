2b45ae974b37671392f8c9a5d9044d55c712ac34
AshkenSC <393940378@qq.com>
2018-07-09 22:54:08 +0800

    Mipmapping parameter tweaked to reduce the over-blurry barrel.

--- src/renderEngine/Loader.java
+++ src/renderEngine/Loader.java
@@ -70,7 +70,7 @@ public class Loader {
 			texture = TextureLoader.getTexture("PNG", new FileInputStream("res/"+fileName+".png"));
 			GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
 			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
-			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_MAX_TEXTURE_LOD_BIAS, -0.4f);
+			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_MAX_TEXTURE_LOD_BIAS, -100.4f);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
