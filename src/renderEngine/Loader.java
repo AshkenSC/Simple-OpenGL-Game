5f5771e00c84f3aa2f5f262d6cce117a62b3b96e
AshkenSC <393940378@qq.com>
2018-07-10 22:52:09 +0800

    Created a new loadToVAO method in Loader class to load vertex positions and texture coordinates of the text data into a VAO.

--- src/renderEngine/Loader.java
+++ src/renderEngine/Loader.java
@@ -43,6 +43,16 @@ public class Loader {
 		return new RawModel(vaoID, indices.length);
 	}
 	
+	public int loadToVAO(float[] positions, float[] textureCoords)
+	{
+		int vaoID = createVAO();
+		storeDataInAttributeList(0, 2, positions);
+		storeDataInAttributeList(1, 2, textureCoords);
+		unbindVAO();
+		//return new RawModel(vaoID, positions.length/3);
+		return vaoID;
+	}
+	
 	public RawModel loadToVAO(float[] positions, float[] textureCoords, float[] normals, float[] tangents, int[] indices)
 	{
 		int vaoID = createVAO();
