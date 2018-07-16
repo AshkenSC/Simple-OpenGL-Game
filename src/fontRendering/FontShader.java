753293a65f80c0c6d4bea1a10642c03675a22615
AshkenSC <393940378@qq.com>
2018-07-16 23:36:02 +0800

    FontShader class updated according to shaders

--- src/fontRendering/FontShader.java
+++ src/fontRendering/FontShader.java
@@ -1,5 +1,8 @@
 package fontRendering;
 
+import org.lwjgl.util.vector.Vector2f;
+import org.lwjgl.util.vector.Vector3f;
+
 import shaders.ShaderProgram;
 
 public class FontShader extends ShaderProgram{
@@ -7,13 +10,17 @@ public class FontShader extends ShaderProgram{
 	private static final String VERTEX_FILE = "src/fontRendering/fontVertex.txt";
 	private static final String FRAGMENT_FILE = "src/fontRendering/fontFragment.txt";
 	
+	private int location_colour;
+	private int location_translation;
+	
 	public FontShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
 	}
 
 	@Override
 	protected void getAllUniformLocations() {
-		
+		location_colour = super.getUniformLocation("colour");
+		location_translation = super.getUniformLocation("translation");
 	}
 
 	@Override
@@ -21,5 +28,12 @@ public class FontShader extends ShaderProgram{
 
 	}
 
+	protected void loadColour(Vector3f colour) {
+		super.loadVector(location_colour, colour);
+	}
+	
+	protected void loadTranslation(Vector2f translation) {
+		super.load2DVector(location_translation, translation);
+	}
 
 }
