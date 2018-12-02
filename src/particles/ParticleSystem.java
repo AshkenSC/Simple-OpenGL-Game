0d57f635ed2be422318bfe117acf9205088d781e
AshkenSC <393940378@qq.com>
2018-12-02 23:09:35 +0800

    (34-1736) Updating particle system class, created generateParticles method

--- src/particles/ParticleSystem.java
+++ src/particles/ParticleSystem.java
@@ -18,6 +18,19 @@ public class ParticleSystem {
 		this.lifeLength = lifeLength;
 	}
 	
+	public void generateParticles(Vector3f systemCenter){
+        float delta = DisplayManager.getFrameTimeSeconds();
+        float particlesToCreate = pps * delta;
+        int count = (int) Math.floor(particlesToCreate);
+        float partialParticle = particlesToCreate % 1;
+        for(int i=0;i<count;i++){
+            emitParticle(systemCenter);
+        }
+        if(Math.random() < partialParticle){
+            emitParticle(systemCenter);
+        }
+    }
+	
 	private void emitParticle(Vector3f center){
         float dirX = (float) Math.random() * 2f - 1f;
         float dirZ = (float) Math.random() * 2f - 1f;
