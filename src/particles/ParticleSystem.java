6736ce092695dd9f894c53e48cc962d2f78490b1
AshkenSC <393940378@qq.com>
2018-12-02 23:08:42 +0800

    (34-1733) Updating particle system class, created emitParticle method

--- src/particles/ParticleSystem.java
+++ src/particles/ParticleSystem.java
@@ -1,5 +1,9 @@
 package particles;
 
+import org.lwjgl.util.vector.Vector3f;
+
+import renderEngine.DisplayManager;
+
 public class ParticleSystem {
 
 	private float pps;
@@ -13,4 +17,13 @@ public class ParticleSystem {
 		this.gravityComplient = gravityComplient;
 		this.lifeLength = lifeLength;
 	}
+	
+	private void emitParticle(Vector3f center){
+        float dirX = (float) Math.random() * 2f - 1f;
+        float dirZ = (float) Math.random() * 2f - 1f;
+        Vector3f velocity = new Vector3f(dirX, 1, dirZ);
+        velocity.normalise();
+        velocity.scale(speed);
+        new Particle(new Vector3f(center), velocity, gravityComplient, lifeLength, 0, 1);
+    }
 }
