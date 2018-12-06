59c5f4813aa2e53ce78b5db793a16acd4e2f65ea
AshkenSC <393940378@qq.com>
2018-12-06 23:29:07 +0800

    (35-0135) Generated getters for variables in ParticleTexture class

--- src/particles/ParticleTexture.java
+++ src/particles/ParticleTexture.java
@@ -10,7 +10,13 @@ public class ParticleTexture {
 		this.textureID = textureID;
 		this.numberOfRows = numberOfRows;
 	}
-	
-	
+
+	public int getTextureID() {
+		return textureID;
+	}
+
+	public int getNumberOfRows() {
+		return numberOfRows;
+	}
 	
 }
