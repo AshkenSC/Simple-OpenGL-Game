245dce7b3227004e377694c47f7741995b5134a6
AshkenSC <393940378@qq.com>
2018-12-06 23:28:22 +0800

    (35-0130) Generated constructor for variables in ParticleTexture class

--- src/particles/ParticleTexture.java
+++ src/particles/ParticleTexture.java
@@ -2,4 +2,15 @@ package particles;
 
 public class ParticleTexture {
 
+	private int textureID;
+	private int numberOfRows;
+	
+	public ParticleTexture(int textureID, int numberOfRows) {
+		super();
+		this.textureID = textureID;
+		this.numberOfRows = numberOfRows;
+	}
+	
+	
+	
 }
