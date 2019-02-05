f3d8f4e9fbae36e7c293ee91b97c576e89247006
AshkenSC <393940378@qq.com>
2019-02-05 23:08:07 +0800

    (35-2000) Set the blend function back to using ALPHA blending in ParticleRenderer

--- src/particles/ParticleRenderer.java
+++ src/particles/ParticleRenderer.java
@@ -86,7 +86,7 @@ public class ParticleRenderer {
 		GL30.glBindVertexArray(quad.getVaoID());
 		GL20.glEnableVertexAttribArray(0);
 		GL11.glEnable(GL11.GL_BLEND);
-		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 		GL11.glDepthMask(false);
 	}
 	
